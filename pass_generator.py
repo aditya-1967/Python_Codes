93e22b9fa7855a25078e6c1ad094618ba1fc4675
Aditya Aman <72183883+aditya-1967@users.noreply.github.com>
2021-09-23 10:56:11 +0530

    Creates Password of Given Length

---
+++ pass_generator.py
@@ -0,0 +1,32 @@
+import random
+
+length = (int)(input("Enter the lenght of password to be genrated: "))
+
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+low_case = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>', '*', '(', ')', '<']
+
+temp_pass_list = []
+password = []
+
+combined_list = digits + low_case + uppercase + symbols
+
+for i in range(10):
+    temp_digits = random.choice(digits) 
+    temp_low_case = random.choice(low_case) 
+    temp_uppercase = random.choice(uppercase)
+    temp_symbols = random.choice(symbols)
+
+    temp_pass = temp_digits + temp_uppercase + temp_symbols + temp_low_case
+    
+
+    for i in range(length - 4):
+        temp_pass += random.choice(combined_list)
+        temp_pass_list.append(temp_pass)
+
+for i in temp_pass_list:
+    if(len(i) == length):
+        password.append(i)
+print("\nHere is the Password list to choose from:\n")
+print(password)
